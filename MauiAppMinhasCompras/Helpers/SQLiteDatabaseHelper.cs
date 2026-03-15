c0cbf86e4ebe7272f57c23ac3f08f409bd976fff
RomanoBoldrin <107365022+RomanoBoldrin@users.noreply.github.com>
2026-03-15 17:06:36 -0300

    Add `SQLiteDatabaseHelper` class to handle SQLite CRUD operations for Produto

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,10 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
 
 namespace MauiAppMinhasCompras.Helpers
 {
-    internal class SQLiteDatabaseHelper
+    public class SQLiteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path) {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto produto) { 
+            return _conn.InsertAsync(produto);
+        }
+
+        public Task<List<Produto>> Update(Produto produto) {
+            string sql = "UPDATE Produto SET Descricao = ?, Quantidade = ?, Preco = ? WHERE Id = ?";
+
+            return _conn.QueryAsync<Produto>(
+                sql, produto.Descricao, produto.Quantidade, produto.Preco, produto.Id
+             );
+        }
+
+        public Task<int> Delete(int id) {
+            return _conn.Table<Produto>().DeleteAsync(p => p.Id == id);
+        }
+
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string querry) {
+            string sql = "SELECT * Produto WHERE Descricao LIKE '%" + querry + "%'";
+
+            return _conn.QueryAsync<Produto>(sql);
+        }
     }
 }
