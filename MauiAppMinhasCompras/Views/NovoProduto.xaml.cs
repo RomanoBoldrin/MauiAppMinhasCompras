00130861f1dbd754b409012c8f82aef189490c5e
RomanoBoldrin <107365022+RomanoBoldrin@users.noreply.github.com>
2026-03-15 21:13:19 -0300

    Add "Novo Produto" page

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -5,5 +7,25 @@ public partial class NovoProduto : ContentPage
 	public NovoProduto()
 	{
 		InitializeComponent();
+	}
+
+	private async void ToolbarItem_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			Produto produto = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_valorUnitario.Text)
+            };
+
+            await App.Database.Insert(produto);
+			await DisplayAlertAsync("Sucesso!", "Registro inserido.", "OK");
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlertAsync("Ops", $"Algo deu errado: {ex.Message}", "OK");
+		}
 	}
 }
