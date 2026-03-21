9ebd079aeb17243e628df0af178b4ee0febe79e2
RomanoBoldrin <107365022+RomanoBoldrin@users.noreply.github.com>
2026-03-21 15:47:59 -0300

    Add `EditarProduto.xaml` and `EditarProduto.xaml.cs`

--- MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class EditarProduto : ContentPage
@@ -6,4 +8,28 @@ public partial class EditarProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto produto_anexado = BindingContext as Produto; // Pega o produto que foi passado como parâmetro na navegação
+
+            Produto produto = new Produto // Pega os dados do formulário e cria um novo produto para atualizar o registro no banco de dados
+            {
+                Id = produto_anexado.Id,
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_valorUnitario.Text)
+            };
+
+            await App.Database.Update(produto);
+            await DisplayAlertAsync("Sucesso!", "Registro Atualizado.", "OK");
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlertAsync("Ops", $"Algo deu errado: {ex.Message}", "OK");
+        }
+    }
 }
